692fd83af0b64469ce0701581265688fb7be8508
yukihirop <te108186@gmail.com>
2019-11-03 18:26:28 +0900

    Implement toppages view

--- viewer/app/controllers/toppages_controller.rb
+++ viewer/app/controllers/toppages_controller.rb
@@ -1,4 +1,6 @@
 class ToppagesController < ApplicationController
   def index
+    @apidoc_title = ApiDoc.title
+    @apidoc_url = ApiDoc.url
   end
 end
---
+++ viewer/app/services/api_doc.rb
@@ -0,0 +1,11 @@
+class ApiDoc
+  class << self
+    def title
+      Settings.apidoc.title
+    end
+
+    def url
+      Settings.apidoc.url
+    end
+  end
+end
