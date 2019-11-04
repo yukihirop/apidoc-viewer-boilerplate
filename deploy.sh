8bb1006720a975287d8c9a3a0aef54ee4c501f38
yukihirop <te108186@gmail.com>
2019-11-04 18:04:37 +0900

    Use r2oas-moneyforward-example instead of r2oas-kubernetes-example when generate api doc
    because kubernetes api document is too large (>5MB).　It takes time to display

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ fi
 APIDOC_SERVER_NAME=${APIDOC_SERVER_NAME} APIDOC_SUBNET_DEFAULT_GATEWAY=${APIDOC_SUBNET_DEFAULT_GATEWAY} IPV4_ADDRESS_VIEWER_BACKEND=${IPV4_ADDRESS_VIEWER_BACKEND} /usr/bin/erb ./apidoc/nginx.conf.erb > ./tmp/nginx.conf
 
 docker-compose down
+docker volume rm apidoc-viewer-boilerplate_build_repository
 CURRENT_DATE=$(date) docker-compose build $1
 docker-compose run viewer_backend rake db:create db:migrate RAILS_ENV=production
 docker-compose up
